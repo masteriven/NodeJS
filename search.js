b8564e18352a63c0f9e38159d0b098a96900acc3
Tal Rimer <rimer3@walla.co.il>
2018-11-20 12:45:47 +0200

    Create search.js

---
+++ search.js
@@ -0,0 +1,47 @@
+
+var path = require('path'), fs = require('fs');
+
+var startPath = '../Hello';
+
+var inputPath = process.argv[2];
+var inputString = process.argv[3]
+
+
+
+function fromDir (startPath,filter) {
+
+	if(!fs.existsSync(startPath)){
+		console.log("no dir",startPath);
+		return;
+}
+		var files = fs.readdirSync(startPath);
+
+	for(var i = 0; i<files.length;i++){
+		var filename = path.join(startPath,files[i]);
+		var stat = fs.lstatSync(filename);
+
+		var readFile = fs.readFileSync(filename, 'utf8');
+		var readFileToString = readFile.toString().trim();
+
+		
+		if(startPath.isDirectory){
+			fromDir(filename,filter);
+
+		}
+		
+		if(readFileToString == inputString){
+			console.log('founds' ,files[i]);
+			
+		}
+	}
+
+	 if(inputString == null && inputPath == null){
+		console.log('USAGE: node search [EXT] [TEXT]');
+	}
+
+	
+};
+fromDir('../Hello',inputPath);
+
+
+ 
